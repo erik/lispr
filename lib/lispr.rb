0b94115b28a6b954b3e855898c98e32ba5702612
Erik Price <erik.price16@gmail.com>
2010-07-03 19:26:16 -0400

    Fixed a problem with LispNumeric

--- lib/lispr.rb
+++ lib/lispr.rb
@@ -14,8 +14,7 @@ module Lispr
   $scope["nil"]   = LispSymbol.new nil
 
   add = lambda do |scope, *args|
-    puts args[0].inspect
-    args[0].each.inject(LispNumeric.new 0) {|x, t| x + t}
+    puts args[0].each.inject(LispNumeric.new 0) {|x, t| x + t}
   end
   $scope["+"]    = add
 end
