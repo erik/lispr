5c0a0f89e6bc828ebd9ab26a94547da5231edf53
Erik Price <erik.price16@gmail.com>
2010-07-09 17:47:30 -0400

    Added some functions

--- lib/lispr/core.lisp
+++ lib/lispr/core.lisp
@@ -4,4 +4,34 @@
 (def defmacro (macro (n a  b) `(def ~n (macro ~a ~b))))
 (defmacro defn (name args body) `(def ~name (fn ~args ~body)))
 
+(defmacro if (test t f)
+ `(cond
+  ~test ~t
+   :else ~f))
+
+;;Numbers
+(defn inc (num) (+ num 1))
+(defn dec (num) (- num 1))
+
+(defmacro zero? (num)
+ `(if (= 0 ~num)
+    true
+    false))
+(defmacro pos? (num)
+ `(if (> ~num 0)
+    true
+    false))
+(defmacro neg? (num)
+ `(if (< ~num 0)
+    true
+    false))
+(defmacro even? (num)
+ `(if (= 0 (% ~num 2))
+    true
+    false))
+(defmacro odd? (num)
+ `(if (even? ~num)
+    false
+    true))
+
 
